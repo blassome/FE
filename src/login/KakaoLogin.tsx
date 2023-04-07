e61afaa24fb8f29c80cafdc445c825203290638a
hayeon <hayun4475@gmail.com>
2023-04-07 20:20:46 +0900

    feat:Get Kakao Social Login Auth Code & User Kakao Infomation(email, nickname, ID)

--- src/login/KakaoLogin.tsx
+++ src/login/KakaoLogin.tsx
@@ -1,45 +1,90 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 import qs from "qs";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const UserName = styled.span`
+  font-weight: bold;
+  font-size: 24px;
+  color: #000080;
+
+  @media (max-width: 480px) {
+    font-size: 16px;
+  }
+`;
 
 function KakaoLogin() {
   const location = useLocation();
-  const AUTH_CODE = location.search.split("=")[1];
+  const [token, setToken] = useState("");
+  const [userName, setUserName] = useState("");
 
-  const getKaKaoToekn = async () => {
-    let tokenResponse;
+  const getToken = async () => {
+    const CLIENT_ID = process.env.REACT_APP_JS_KEY;
+    const REDIRECT_URI = process.env.REACT_APP_REDIRECT_URI;
+    const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET;
+    const code = location.search.split("=")[1];
+    const payload = qs.stringify({
+      grant_type: "authorization_code",
+      client_id: CLIENT_ID,
+      redirect_uri: REDIRECT_URI,
+      code: code,
+      client_secret: CLIENT_SECRET,
+      "Content-Type": "application/x-www-form-urlencoded ",
+    });
+
     try {
-      // Authorization Server로부터 Access Token 발급받기
-      tokenResponse = await axios({
-        method: 'POST',
-        url: 'https://kauth.kakao.com/oauth/token',
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded'
-        },
-        data: qs.stringify({
-          'grant_type': 'authorization_code',
-          'client_id': process.env.REACT_APP_REST_API_KEY,
-          'client_secret': process.env.REACT_APP_KAKAO_CLIENT_SECRET,
-          'redirect_uri': process.env.REACT_APP_REDIRECT_URI,
-          'code': AUTH_CODE
-        })
-      });
+      //토큰 가져오기
+      const res = await axios.post(
+        "https://kauth.kakao.com/oauth/token",
+        payload
+      );
+      setToken(res.data.access_token);
+      console.log(res.data.access_token);
     } catch (error) {
-      return console.log("에러남");
+      console.log("에러남");
     }
-    console.info(tokenResponse.data);
   };
 
   useEffect(() => {
-    if (!location.search) return;
-    getKaKaoToekn();
-  }, []);
+    getToken();
+  });
+
+  useEffect(() => {
+    const getKakaoUser = async () => {
+      if (token) {
+        try {
+          //토큰을 이용하여 사용자 정보 가져오기
+          const kakaoUser = await axios.get(
+            `https://kapi.kakao.com/v2/user/me`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
+          setUserName(kakaoUser.data.properties.nickname);
+        } catch (error) {
+          console.log("에러남");
+        }
+      }
+    };
+    getKakaoUser();
+  }, [token]);
 
   return (
-    <div>
-      <h1>로그인 완료 페이지</h1>
-    </div>
+    // 사용자 정보 출력
+    <Wrapper>
+      {userName && <UserName>안녕하세요 {userName}님!</UserName>}
+    </Wrapper>
   );
 }
 
--- src/login/StartLogin.tsx
+++ src/login/StartLogin.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import styled from "styled-components";
-import KakaoLogin from "./KakaoLogin";
 
 const KakaoLoginButtonWrapper = styled.div`
   display: flex;
